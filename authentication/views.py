b52781cc0c5bacbd6f95d75cfce12b026147b072
Gabuchis <gabrielrosero180106@gmail.com>
2025-10-03 12:27:36 -0500

    Import http response

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
 from django.contrib import messages
 from .models import Country
 from .forms import CountryForm
