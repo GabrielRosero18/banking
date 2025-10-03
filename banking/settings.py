15b5a8f43ccb3ca17dcd6b504d2210ca3c1cd86d
Gabuchis <gabrielrosero180106@gmail.com>
2025-10-03 12:11:43 -0500

    Country templates for UI2

--- banking/settings.py
+++ banking/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'banking.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # agrega esta línea
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -140,7 +140,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']  # carpeta para css/js
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # para collectstatic en producción
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
